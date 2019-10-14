4e70e80bebd37b48cdbaaa48defe1799a58a639a
thepractitioner1 <35992700+thepractitioner1@users.noreply.github.com>
2019-10-14 16:26:16 +0100

    final bug fix

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function () {
-  const db = "mongodb+srv://misan:1234@cluster0-34d9x.mongodb.net/test?retryWrites=true&w=majority"
+  const db = "mongodb://misan:1234@cluster0-shard-00-00-34d9x.mongodb.net:27017,cluster0-shard-00-01-34d9x.mongodb.net:27017,cluster0-shard-00-02-34d9x.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
