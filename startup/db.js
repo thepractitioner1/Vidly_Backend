6e4fc542c6249ba173143deddfccc82a0016b7ce
thepractitioner1 <35992700+thepractitioner1@users.noreply.github.com>
2019-10-14 15:55:45 +0100

    final bug fix

--- startup/db.js
+++ startup/db.js
@@ -2,8 +2,8 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
-module.exports = function() {
-  const db = config.get('db');
+module.exports = function () {
+  const db = "mongodb+srv://DavidMisan:David1@2@cluster0-34d9x.mongodb.net/test?retryWrites=true&w=majority"
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
