44377f96f943690884b13c5c95940fe56d8180aa
mokerentie <mokerentie@paga.com>
2020-08-04 13:45:31 +0100

    Added script to upload docker image

---
+++ upload_docker.sh
@@ -0,0 +1,17 @@
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="thepract/vidly_backend_web"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag vidly_backend_web $dockerpath:latest
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:latest
